4056000981896d268ec2fa3690c7614054f4f860
roufurrohim <77450299+roufurrohim@users.noreply.github.com>
2021-10-10 09:47:12 +0700

    Revert "adding pages hirepage, login, reset pass, chat, and components hire and chatlist"

--- src/Route/index.js
+++ src/Route/index.js
@@ -1,12 +1,7 @@
 import { Switch, Route } from 'react-router-dom'
 import Landingpage from '../Pages/Landingpage'
-import Login from '../Pages/Login'
-import Reset from '../Pages/Resetpw'
-import Hire from '../Pages/Hirepage'
-import Chat from '../Pages/Chat'
 import Home from '../Pages/Home'
 import Guard from './guard'
-
 import CompanyProfiles from '../Pages/Profile'
 import CompanyEditProfiles from '../Pages/EditProfile'
 
@@ -16,12 +11,6 @@ const Router =()=>{
         <Route path="/" exact>
           <Landingpage />
         </Route>
-        <Route path="/login" exact render={(props)=>(<Login  {...props}/>)}/>
-        <Route path="/reset-password" exact render={(props)=>(<Reset  {...props}/>)}/>
-        <Route path="/hire" exact render={(props)=>(<Hire  {...props}/>)}/>
-        {/* <Route path="/message" exact render={(props)=>(<Chat  {...props}/>)}/> */}
-        <Guard path="/hire" exact component={Hire}/>
-        <Guard path="/message" component={Chat}/>
 
         <Guard path="/home" component={Home} />
         <Guard path="/my-profile" component={CompanyProfiles} />
--- src/helpers/var.js
+++
@@ -1,7 +0,0 @@
-// user
-export const getAllUser = 'GET_ALL_USER_FULFILLED'
-export const getAllUserPending = 'GET_ALL_USER_PENDING'
-export const getAllUserError = 'GET_ALL_USER_REJECTED'
-export const getDetailUser = 'GET_DETAIL_USER_FULFILLED'
-export const getDetailUserPending = 'GET_DETAIL_USER_PENDING'
-export const getDetailUserError = 'GET_DETAIL_USER_REJECTED'
