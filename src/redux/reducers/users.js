a3f90d68a4d0becb7c9a8c6aff50834067a8b3e7
@farizian <83340630+farizian@users.noreply.github.com>
2021-10-12 19:27:54 +0700

    add mydetails

--- src/redux/reducers/users.js
+++ src/redux/reducers/users.js
@@ -7,6 +7,14 @@ const initialState = {
     loadDetails: false,
     errorDetails: false,
     errorDetailsMessage: "Data Not Found",
+    details2: {},
+    loadDetails2: false,
+    errorDetails2: false,
+    errorDetailsMessage2: "Data Not Found",
+    mydetails: {},
+    loadMyDetails: false,
+    errorMyDetails: false,
+    errorMyDetailsMessage: "Data Not Found",
 }
 
 const usersReducer = (state=initialState, action) => {
@@ -52,7 +60,46 @@ const usersReducer = (state=initialState, action) => {
                 loadDetails: false,
                 errorDetailsMessage: action.payload
             }
-    
+        case "GET_DETAILS2_USER_PENDING":
+        return {
+            ...state,
+            loadDetails2: true
+        }
+
+      case "GET_DETAILS2_USER_FULLFILLED":
+          return {
+              ...state,
+              loadDetails2: false,
+              details2: action.payload,
+              errorDetailsMessage2: "Get User Success"
+          }
+
+      case "GET_DETAILS2_USER_REJECTED":
+          return {
+              ...state,
+              loadDetails2: false,
+              errorDetailsMessage2: action.payload
+          }
+      case "GET_MYDETAILS_USER_PENDING":
+        return {
+            ...state,
+            loadMyDetails: true
+        }
+  
+        case "GET_MYDETAILS_USER_FULLFILLED":
+            return {
+                ...state,
+                loadMyDetails: false,
+                mydetails: action.payload,
+                errorMyDetailsMessage: "Get User Success"
+            }
+  
+        case "GET_MYDETAILS_USER_REJECTED":
+            return {
+                ...state,
+                loadMyDetails: false,
+                errorMyDetailsMessage: action.payload
+            }
         default:
             return state
     }
