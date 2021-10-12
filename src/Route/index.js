5c3b6b758f6a1f135c04265aea393b5663ca48a8
@farizian <83340630+farizian@users.noreply.github.com>
2021-10-13 01:38:34 +0700

    add reset pw and forgetpw

--- src/Route/index.js
+++ src/Route/index.js
@@ -3,7 +3,6 @@ import Landingpage from '../Pages/Landingpage'
 import Login from '../Pages/Login'
 import Reset from '../Pages/Resetpw'
 import Hire from '../Pages/Hirepage'
-// import Chat from '../Pages/Chat'
 import Profile from '../Pages/Profile2'
 import EditProfile from '../Pages/EditProfile'
 import Home from '../Pages/Home'
@@ -19,20 +18,19 @@ const Router =()=>{
             <Landingpage />
         </Route>
         
-        <Route path="/Konfirmasipass" exact render={(props)=>(<Konfirmasipass  {...props}/>)}/>
+        <Route path="/reset-password" exact render={(props)=>(<Konfirmasipass  {...props}/>)}/>
         <Route path="/register/:id" exact render={(props)=>(<Register  {...props}/>)}/>
         <Guard path="/editprofile/:id" exact component={EditProfile}/>
         <Route path="/login" exact render={(props)=>(<Login  {...props}/>)}/>
         <Route path="/login/:id" exact render={(props)=>(<Login  {...props}/>)}/>
-        <Route path="/reset-password" exact render={(props)=>(<Reset  {...props}/>)}/>
+        <Route path="/forget-password" exact render={(props)=>(<Reset  {...props}/>)}/>
         <Guard path="/hire" exact render={(props)=>(<Hire  {...props}/>)}/>
         {/* <Route path="/message" exact render={(props)=>(<Chat  {...props}/>)}/> */}
-        <Guard path="/hirepage" exact component={Hire}/>
+        {/* <Guard path="/hire" exact component={Hire}/> */}
         <Guard path="/message" component={Message}/>
-        <Guard path="/editprofile/:id" exact component={EditProfile}/>
         <Guard path="/profile" exact component={Profile}/>
         <Guard path = "/home" component={Home} />
-
+            
         <Route>
           404 NOT FOUND
         </Route>
